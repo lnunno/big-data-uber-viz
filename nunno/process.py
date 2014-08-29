4de5d5284f7e06aa36208a3dc0714fbff8726486
lnunno <lucasnunno@gmail.com>
2014-08-28 21:04:06 -0600

    Filtering by day of the week now works.

--- nunno/process.py
+++ nunno/process.py
@@ -4,7 +4,6 @@ Requires:
 * python3
 * pandas - pip3 install pandas
 
-
 Created on Aug 28, 2014
 
 @author: lnunno
@@ -25,10 +24,23 @@ def load_file(load_type=None, day_filters=None, nrows=None):
     uber_file_path = '../uber_gps_tsv/all_header.tsv'
     df = pd.read_table(uber_file_path, sep='\t', nrows=nrows)
     df['timestamp'] = pd.to_datetime(df['timestamp'])
+    day_of_week_index = pd.Index(df['timestamp']).dayofweek
+    if day_filters:
+        # Filter by the day(s) of the week.
+        def filter_by_day_of_week(days, day_ls):
+            ls = []
+            for x in days:
+                if x in day_ls:
+                    ls.append(True)
+                else:
+                    ls.append(False)
+            return ls
+        df = df[filter_by_day_of_week(day_of_week_index, day_filters)]
     return df
 
 def main():
-    df = load_file(nrows=1000)
+    day_map = {"M":0,"T":1,"W":2,"R":3,"F":4,"S",5,"U":6}
+    df = load_file(nrows=1000, day_filters=[1])
         
 if __name__ == '__main__':
     main()
