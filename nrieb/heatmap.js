d0863e7dacb924986b49bfaf3e2bff5081891bc6
Nathan Rieb <nrieb@cs.unm.edu>
2014-08-31 23:56:39 -0600

    Day seems to work now. 100k random sampling of location data.

--- nrieb/heatmap.js
+++ nrieb/heatmap.js
@@ -1,88 +1,43 @@
-/*
 var map;
+var SAMPLE_SIZE = 100000
 
-function initialize() {
-    var mapOptions = {
-	zoom: 13,
-	center: new google.maps.LatLng(37.77, -122.4),
-	mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-		
-    map = new google.maps.Map(document.getElementById('map_canvas'),
-			      mapOptions);
-		
-    // Create a script tag and set the USGS URL as the source.
-    // Append this tag to the document's <head>.
-    var script = document.createElement('script');
-    script.src = 'http://earthquake.usgs.gov/earthquakes/feed/geojsonp/2.5/week';
-    document.getElementsByTagName('head')[0].appendChild(script);
-}
-		
-window.eqfeed_callback = function(results) {
-    var heatmapData = [];
-    for (var i = 0; i < results.features.length; i++) {
-	var coords = results.features[i].geometry.coordinates;
-	var latLng = new google.maps.LatLng(coords[1], coords[0]);
-
-	heatmapData.push(latLng);
+// See https://stackoverflow.com/questions/11935175/sampling-a-random-subset-from-an-array
+function getRandomSubarray(arr, size) {
+    var shuffled = arr.slice(0), i = arr.length, min = i - size, temp, index;
+    if (min < 0)
+	min = 0
+    while (i-- > min) {
+        index = Math.floor((i + 1) * Math.random());
+        temp = shuffled[index];
+        shuffled[index] = shuffled[i];
+        shuffled[i] = temp;
     }
-    var heatmap = new google.maps.visualization.HeatmapLayer({
-	data: heatmapData,
-	dissipating: false,
-	map: map
-    });
+    return shuffled.slice(min);
 }
-*/
-/* Data points defined as an array of LatLng objects */
-
-var map;
 
 function success(data, textStatus, jqXHR) {
-    var heatMapData;
-    for (tripid in jsonData)
-	for (i in jsonData[tripid])
-	    heatmapData.push(new google.maps.LatLng(jsonData[tripid][i][0],
-						    jsonData[tripid][i][1]))
-
-
+    var heatMapData = [];
+    console.log("got a success");
+    for (tripid in data)
+	for (i in data[tripid])
+		heatMapData.push(new google.maps.LatLng(data[tripid][i][1],
+							data[tripid][i][2]))
+    sample = getRandomSubarray(heatMapData, SAMPLE_SIZE)
     var heatmap = new google.maps.visualization.HeatmapLayer({
-	data: heatmapData,
-	map: map
+	data: sample,
+	radius: 7
     });
-
-
+    heatmap.setMap(map)
 }
 
 function initialize() {
-
+    Math.seedrandom('a better seed?')
     $.ajax({
         url: "http://cs.unm.edu/~lnunno/uber-viz/json/fileLoader.php?name=night.json",
         dataType:"json",
         async: true,
-	success: success
+	success: success,
     });
-
-
-    var heatmapData = [
-	new google.maps.LatLng(37.782, -122.447),
-	new google.maps.LatLng(37.782, -122.445),
-	new google.maps.LatLng(37.782, -122.443),
-	new google.maps.LatLng(37.782, -122.441),
-	new google.maps.LatLng(37.782, -122.439),
-	new google.maps.LatLng(37.782, -122.437),
-	new google.maps.LatLng(37.782, -122.437),
-	new google.maps.LatLng(37.782, -122.437),
-	new google.maps.LatLng(37.782, -122.437),
-	new google.maps.LatLng(37.782, -122.437),
-	new google.maps.LatLng(37.782, -122.435),
-	new google.maps.LatLng(37.785, -122.447),
-	new google.maps.LatLng(37.785, -122.445),
-	new google.maps.LatLng(37.785, -122.443),
-	new google.maps.LatLng(37.785, -122.441),
-	new google.maps.LatLng(37.785, -122.439),
-	new google.maps.LatLng(37.785, -122.437),
-	new google.maps.LatLng(37.785, -122.435)
-    ];
 
     var sanFrancisco = new google.maps.LatLng(37.774546, -122.433523);
     
@@ -90,7 +45,6 @@ function initialize() {
 	center: sanFrancisco,
 	zoom: 13,
 	zoomControl: true,
-	mapTypeId: google.maps.MapTypeId.TRAFFIC
+	mapTypeId: google.maps.MapTypeId.ROADMAP,
     });
-
 }
